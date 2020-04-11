f344366508a0200fbc77926bd7a2c77755f02406
Soumik Sarkar <soumiksarkar.3120@gmail.com>
2020-04-11 23:00:52 +0530

    Add a browser action and change the icon to svg. (#3)

---
+++ carrot/src/popup/popup.js
@@ -0,0 +1,15 @@
+function setup() {
+    const manifest = browser.runtime.getManifest();
+
+    document.querySelector('#version').textContent = 'v' + manifest.version;
+    document.querySelector('#title').textContent = manifest.name;
+    document.querySelector('#icon').src = manifest.browser_action.default_icon;
+
+    const settings = document.querySelector('#settings');
+    settings.addEventListener('click', () => {
+        browser.runtime.openOptionsPage();
+        window.close();
+    });
+}
+
+document.addEventListener('DOMContentLoaded', setup);
