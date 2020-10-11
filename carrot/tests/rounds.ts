3436b8db4bf1d1b58c22f8ca4b0daa2cffdf0a54
meooow <soumiksarkar.3120@gmail.com>
2020-10-11 13:05:51 +0530

    Add code to download round data
    Update existing round data using the new script

--- carrot/tests/rounds.ts
+++ carrot/tests/rounds.ts
@@ -1,4 +1,5 @@
-import { Contestant } from "../src/background/predict.js";
+import { Contestant } from '../src/background/predict.js';
+import * as api from '../src/background/cf-api.js';
 
 const DATA_FILE_REGEX = /^(round-.*)-data.json$/;
 
@@ -17,9 +18,8 @@ export class RoundData {
 }
 
 function readDataFromFile(file: string | URL): DataRow[] {
-  const bytes = Deno.readFileSync(file);
-  const json  = JSON.parse(new TextDecoder("utf-8").decode(bytes));
-  return json.map((j: [string, number, number, number, number]) => new DataRow(...j));
+  const json: [string, number, number, number, number][] = JSON.parse(Deno.readTextFileSync(file));
+  return json.map((j) => new DataRow(...j));
 }
 
 export function dataRowsToContestants(testData: DataRow[]): Contestant[] {
@@ -33,4 +33,29 @@ export function readTestData(): RoundData[] {
         new RoundData(
             entry.name.match(DATA_FILE_REGEX)![1].replaceAll('-', '_'),
             readDataFromFile(new URL(entry.name, import.meta.url))));
+}
+
+async function main() {
+  const contestId = Deno.args[1];
+  if (Deno.args[0] != 'download' || !contestId) {
+    console.error('Usage: deno run --allow-net --allow-write rounds.ts download <contestId>');
+    Deno.exit(1);
+  }
+
+  const { rows } = await api.contest.standings(contestId);
+  const rowMap = new Map<string, any>(rows.map((r: any) => [r.party.members[0].handle, r]));
+
+  const changes = await api.contest.ratingChanges(contestId);
+
+  const output = changes.map((c: any) => {
+    const row = rowMap.get(c.handle);
+    return [c.handle, row.points, row.penalty, c.oldRating, c.newRating - c.oldRating];
+  });
+
+  const fileName = `round-${contestId}-data.json`;
+  Deno.writeTextFileSync(new URL(fileName, import.meta.url), JSON.stringify(output));
+}
+
+if (import.meta.main) {
+  main();
 }
